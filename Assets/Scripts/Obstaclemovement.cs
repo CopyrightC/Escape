8e4ab385eac93c15ebc133659eb3744eb5dc2ae6
Anonymous_Programmer <68592417+SATAN01@users.noreply.github.com>
2021-02-06 22:40:25 +0530

    Add files via upload

---
+++ Assets/Scripts/Obstaclemovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaclemovement : MonoBehaviour
+{
+    float period = 2f;
+    // Start is called before the first frame update
+    [SerializeField] Vector3 movementvector;
+    Vector3 startingpos;
+    [Range(0,1)][SerializeField] float movementslide;
+    void Start()
+    {
+        startingpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float sinwave = Mathf.Sin(cycles * tau);
+        movementslide = (sinwave /2f) + 0.5f;
+        Vector3 changepos = movementslide*movementvector;
+        transform.position = startingpos + changepos;
+    }
+}
---
+++ Assets/Scripts/Rocketmovment.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Rocketmovment : MonoBehaviour
+{
+    [SerializeField] float rscthrs = 70f;
+    [SerializeField] float speedthr = 100f;
+    [SerializeField] AudioClip ThrustSnd;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip levelcross;
+    [SerializeField] ParticleSystem Rockpart;
+    [SerializeField] ParticleSystem Lvlpart;
+    [SerializeField] ParticleSystem Deathpart;
+    enum gamestate {Alive,Dead,Changing};
+    gamestate state = gamestate.Alive;
+    Rigidbody rockrigi;
+    AudioSource audiosrc;
+
+    void Start()
+    {
+        audiosrc = GetComponent<AudioSource>();
+        rockrigi = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (state == gamestate.Alive){
+            HandleThrust();
+            checkRotation();
+        }
+        
+    }
+    void OnCollisionEnter(Collision collision){
+        if (state!=gamestate.Alive){return;}
+        switch(collision.gameObject.tag){
+            case "Friendly":
+                Debug.Log("OK");
+                break;
+            case "Finish":
+                state = gamestate.Changing;
+                audiosrc.Stop();
+                audiosrc.PlayOneShot(levelcross);
+                Lvlpart.Play();
+                Invoke("LoadnxtScene",1f);
+                break;
+            case "Death":
+                state = gamestate.Dead;
+                audiosrc.Stop();
+                audiosrc.PlayOneShot(deathSound);
+                Deathpart.Play();
+                Invoke("previousscene",1f);
+                break;
+            default: 
+                break;
+            
+        }
+    }
+    void LoadnxtScene() {
+        int lvlindex = SceneManager.GetActiveScene().buildIndex;
+        int nxtindex = lvlindex+1;
+        if (nxtindex == 5){
+            nxtindex = 0;
+        }
+        SceneManager.LoadScene(nxtindex);
+    }
+    void previousscene(){
+        SceneManager.LoadScene(0);
+    }
+    private void HandleThrust(){
+        if (Input.GetKey(KeyCode.Space)){
+            rockrigi.AddForce(Vector3.up * speedthr);
+            if (!audiosrc.isPlaying){
+                audiosrc.PlayOneShot(ThrustSnd);
+            }
+            Rockpart.Play();
+        }
+        else{
+            audiosrc.Stop();
+            Rockpart.Stop();
+        }
+        
+    }
+    private void checkRotation(){
+        
+        rockrigi.freezeRotation = true;
+        float speed = rscthrs*Time.deltaTime;
+        if (Input.GetKey(KeyCode.A)){
+          
+            rockrigi.AddForce(Vector3.left*rscthrs);
+        }
+        else if (Input.GetKey(KeyCode.D)){
+          
+            rockrigi.AddForce(Vector3.right*rscthrs);
+        }
+        rockrigi.freezeRotation = false;
+    }
+}
